b00d0d958f68965403ba4fa845aabeb99d294caf
Luis Miguel Del Gallego <alucardluis@gmail.com>
2021-02-20 13:30:10 -0500

    first commit

---
+++ lib/src/pages/tabs_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class TabsPage extends StatelessWidget {
+  const TabsPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        physics: BouncingScrollPhysics(),
+        children: [
+          Container(
+            color: Colors.red,
+          ),
+          Container(
+            color: Colors.green,
+          ),
+        ],
+      ),
+    );
+  }
+}
