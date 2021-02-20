54980d2b358b572635842cc3130c491829f68a5f
Luis Miguel Del Gallego <alucardluis@gmail.com>
2021-02-20 13:50:10 -0500

    Implementando BottonNavigationBar

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -6,17 +6,44 @@ class TabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        physics: BouncingScrollPhysics(),
-        children: [
-          Container(
-            color: Colors.red,
-          ),
-          Container(
-            color: Colors.green,
-          ),
-        ],
-      ),
+      body: _Pages(),
+      bottomNavigationBar: _ButtonNavigationBar(),
+    );
+  }
+}
+
+class _ButtonNavigationBar extends StatelessWidget {
+  const _ButtonNavigationBar({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      items: [
+        BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline), label: 'Para ti'),
+        BottomNavigationBarItem(icon: Icon(Icons.public), label: 'Encabezados'),
+      ],
+    );
+  }
+}
+
+class _Pages extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      // physics: BouncingScrollPhysics(),
+      physics: NeverScrollableScrollPhysics(),
+      children: [
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.green,
+        ),
+      ],
     );
   }
 }
