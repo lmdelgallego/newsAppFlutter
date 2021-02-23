34fa22e1264affd52a9d2ce32f0be3e9a5075924
Luis Miguel Del Gallego <alucardluis@gmail.com>
2021-02-23 07:44:50 -0500

    :bento: Menú categorias

--- lib/src/pages/tab2_page.dart
+++ lib/src/pages/tab2_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:newsapp/src/models/category_model.dart';
+import 'package:newsapp/src/services/news_service.dart';
+import 'package:newsapp/src/utils/utils.dart';
+import 'package:provider/provider.dart';
 
 class Tab2Page extends StatefulWidget {
   const Tab2Page({Key key}) : super(key: key);
@@ -11,11 +15,71 @@ class _Tab2PageState extends State<Tab2Page>
     with AutomaticKeepAliveClientMixin {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text('Tab 2 Page')),
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(child: _ListCategory()),
+          ],
+        ),
+      ),
     );
   }
 
   @override
   bool get wantKeepAlive => true;
+}
+
+class _ListCategory extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final categories = Provider.of<NewsService>(context).categories;
+
+    return ListView.builder(
+      physics: BouncingScrollPhysics(),
+      scrollDirection: Axis.horizontal,
+      itemCount: categories.length,
+      itemBuilder: (BuildContext context, int index) {
+        final category = categories[index];
+        return _CategoryBotton(category: category);
+      },
+    );
+  }
+}
+
+class _CategoryBotton extends StatelessWidget {
+  const _CategoryBotton({
+    Key key,
+    @required this.category,
+  }) : super(key: key);
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          GestureDetector(
+            onTap: () {
+              print('${category.name}');
+            },
+            child: Container(
+              width: 40,
+              height: 40,
+              margin: EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+              ),
+              child: Icon(category.icon, color: Colors.black54),
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(capitalizeText(category.name)),
+        ],
+      ),
+    );
+  }
 }
--- lib/src/services/news_service.dart
+++ lib/src/services/news_service.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:newsapp/src/models/category_model.dart';
 import 'package:newsapp/src/models/news_models.dart';
 import 'package:http/http.dart' as http;
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 final _URL_NEWS = 'https://newsapi.org/v2';
 final _APIKEY = '80af870410884d14822a46eb883d66a5';
 
 class NewsService with ChangeNotifier {
   List<Article> headlines = [];
+  List<Category> categories = [
+    Category(FontAwesomeIcons.building, 'business'),
+    Category(FontAwesomeIcons.tv, 'entertainment'),
+    Category(FontAwesomeIcons.addressCard, 'general'),
+    Category(FontAwesomeIcons.headSideCough, 'health'),
+    Category(FontAwesomeIcons.vials, 'science'),
+    Category(FontAwesomeIcons.volleyballBall, 'sports'),
+    Category(FontAwesomeIcons.memory, 'technology'),
+  ];
 
   NewsService() {
     this.getTopHeadlines();
